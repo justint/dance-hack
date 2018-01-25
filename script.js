08c8082b9732ae94a9a2e217a7d3e5005d8cde8c
Justin Tennant <justin.tennant@sjsu.edu>
2018-01-25 00:53:11 -0800

    Add text, 'see more', videos

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 $(document).ready( function(){
 	$("#logo").delay(200).fadeTo(1800, 1);
 
-	$(".github-button").delay(1200).fadeTo(1800, 1);
+	$("#title").delay(1200).fadeTo(1000, 1);
 
-	//not fading these in until more is completed
-	//$("#title").delay(3000).fadeTo(1000, 1);
-	//$(".paragraph").delay(3000).fadeTo(1800, 1);
+	$(".github-button").delay(2600).fadeTo(1800, 1);
+
+	$(".paragraph").delay(2600).fadeTo(1800, 1);
+
+	$(".b_seemore").delay(3000).fadeTo(1800, 1);
+
 })
+
+function show_more() {
+	$(".media").fadeTo(700, 1);
+
+	$('html, body').animate({
+        scrollTop: $(".media").offset().top
+    }, 2000);
+}
